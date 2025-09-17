03cc86348a86cfd6bf2b46fcf13f057f252ce3af
Andreas Fuchs <asf@boinkor.net>
2025-09-17 10:37:09 -0400

    nix fmt

--- dev/flake-part.nix
+++ dev/flake-part.nix
@@ -2,8 +2,7 @@
   self,
   inputs,
   ...
-}:
-{
+}: {
   imports = [
     inputs.devshell.flakeModule
     inputs.generate-go-sri.flakeModules.default
@@ -13,36 +12,34 @@
     "aarch64-darwin"
   ];
 
-  perSystem =
-    {
-      config,
-      pkgs,
-      system,
-      flocken,
-      ...
-    }:
-    {
-      go-sri-hashes.wait-for-interfaces = { };
+  perSystem = {
+    config,
+    pkgs,
+    system,
+    flocken,
+    ...
+  }: {
+    go-sri-hashes.wait-for-interfaces = {};
 
-      devshells.default = {
-        commands = [
-          {
-            name = "regenSRI";
-            category = "dev";
-            help = "Regenerate wait-for-interfaces.sri in case the module SRI hash should change";
-            command = "${config.apps.generate-sri-wait-for-interfaces.program}";
-          }
-        ];
-        packages = [
-          pkgs.go
-          pkgs.gopls
-          pkgs.golangci-lint
-        ];
-      };
-
-      apps = {
-        default = config.apps.wait-for-interfaces;
-        wait-for-interfaces.program = config.packages.wait-for-interfaces;
-      };
+    devshells.default = {
+      commands = [
+        {
+          name = "regenSRI";
+          category = "dev";
+          help = "Regenerate wait-for-interfaces.sri in case the module SRI hash should change";
+          command = "${config.apps.generate-sri-wait-for-interfaces.program}";
+        }
+      ];
+      packages = [
+        pkgs.go
+        pkgs.gopls
+        pkgs.golangci-lint
+      ];
     };
+
+    apps = {
+      default = config.apps.wait-for-interfaces;
+      wait-for-interfaces.program = config.packages.wait-for-interfaces;
+    };
+  };
 }
